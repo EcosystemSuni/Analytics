5e5a7cf2ddd3a1f7e0ef23c7ecd84d545c957f4a
Edinsoncs <edinsoncode@gmail.com>
2021-10-02 22:46:29 +0300

    build

--- out/_next/static/9mrWWdHQrcsSWfqI8JrLf/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fpairs\u002Flosers","\u002F","\u002Fpairs","\u002Fpools\u002Frecent","\u002Fpools","\u002Ftokens","\u002Fbar","\u002Fpairs\u002Frecent","\u002Fpairs\u002Fgainers","\u002Fpools\u002F[id]","\u002Ftokens\u002F[id]","\u002Fpairs\u002F[id]","\u002Fusers\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
